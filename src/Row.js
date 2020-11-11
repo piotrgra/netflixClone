304d5069af80e809c2d33790322d541448cedd12
Piotr <grabowskipiotr1989@gmail.com>
2020-11-11 12:55:23 +0100

    netflix clone

---
+++ src/Row.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
+import movieTrailer from "movie-trailer";
+import ReactPlayer from "react-player";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+
+    fetchData();
+  }, [fetchUrl]);
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.title || movie?.name || movie?.original_name || "")
+        .then((url) => {
+          setTrailerUrl(url);
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            className={`row__poster ${isLargeRow && "row_posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie?.title || movie?.name || movie?.original_name}
+            key={movie.id}
+            onClick={() => handleClick(movie)}
+          />
+        ))}
+      </div>
+      {trailerUrl && <ReactPlayer url={trailerUrl} />}
+    </div>
+  );
+}
+
+export default Row;
